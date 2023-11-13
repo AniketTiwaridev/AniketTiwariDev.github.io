3068fbaf7ca2ef2dba7f977ce5f064dfb2b160e6
AniketTiwaridev <149860593+AniketTiwaridev@users.noreply.github.com>
2023-11-14 00:05:38 +0530

    Create resume.php

---
+++ resume.php
@@ -0,0 +1,24 @@
+<?php
+$name="Aniket Tiwari";
+$H_S_S="Gagan Bharti Sr. Sec. School";
+$S_S="Government Sarvodaya Bal Vidyalaya";
+$country="INDIA";
+$state="Delhi";
+$City="New Delhi";
+$District="West Delhi";
+$Town="Uttam Nagar";
+$Area="Nand Ram Park";
+$floor_no="A-115 2nd Floor";
+$pincode="110059";
+$phone="+91-8750116640";
+$whatsapp="8802042444";
+$email="aniket.tiwari136693@gmail.com";
+$total_exp ="1 Year 10 Months";
+$synopsisias_exp="March,2020 - June,2020";
+$tripzeella="September, 2021 - March, 2022";
+$Webkeyindia="November, 2022 - May, 2023";
+
+
+
+
+?>
