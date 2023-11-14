44b8673c06264c166c8a4fc6074ce67b383005ce
AniketTiwaridev <149860593+AniketTiwaridev@users.noreply.github.com>
2023-11-14 15:10:25 +0530

    Update resume.php

--- resume.php
+++ resume.php
@@ -19,6 +19,264 @@ $tripzeella="September, 2021 - March, 2022";
 $Webkeyindia="November, 2022 - May, 2023";
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Aniket Tiwari Resume </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <style type="text/css">
+    * {
+      margin: 0;
+      padding: 0;
+    }
 
+    .primary-info {
+      color: white;
+      /*padding: 25px;*/
+    }
+
+ /*   .secondary-info {
+      color: white;
+      padding-top: 25px;
+    }*/
+
+    ul li {
+      list-style: none;
+    }
+    p
+    {
+      font-family: Georgia;
+      text-align: justify;
+    }
+      hr
+      {
+        color: blue;
+      
+        border: solid #5b5b76;
+      }
+    .float-right{
+      float: right;
+    }
+    .profile-img
+    {
+      width: 150px;
+      height:129px;
+    }
+    .profile-img img {
+      max-width: 100%;
+      height: auto;
+    }
+    .work-exp span
+    {
+      font-weight: bold;
+      font-size: 15px;
+    }
+    .social-media
+    {
+      padding-left: 20px;
+      width: 140px;
+    }
+    .social-media i
+    {
+      color: white;
+      margin-right: 11px;
+      margin-top: 15px;
+
+    }
+    h3
+    {
+      font-family: agency fb;
+      font-weight: bolder;
+    }
+    .desc
+    {
+      text-align: justify;
+      font-style: 'Roboto', sans-serif;
+    }
+
+     @media print {
+            body {
+                size: A3; /* Set paper size to A3 */
+            }
+
+            /* Hide the print button in the PDF */
+            button {
+                display: none;
+            }
+        }
+  </style>
+</head>
+<body>
+
+<div class="container">
+  <div class="row mt-2">
+    <div class="col-4 bg-dark">
+      <div class="profile-img pt-2">
+        <img class="img-fluid" src="https://t4.ftcdn.net/jpg/02/90/27/39/360_F_290273933_ukYZjDv8nqgpOBcBUo5CQyFcxAzYlZRW.jpg">
+      </div>
+      <div class="social-media ">
+        <i class="fa-brands fa-github"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-codepen"></i>
+        <i class="fa-solid fa-globe"></i>
+      </div>
+    </div>
+
+    <div class="col-8 bg-dark">
+      <div class="primary-info float-right">
+         <h2><strong>Aniket Tiwari</strong></h2>
+              <h5>Web Developer</h5>
+              <ul>
+                <li><span><i class="fa-regular fa-envelope"></i> </span>dev.aniket.tiwari@gmail.com</li>
+                <li><span><i class="fa-solid fa-phone"></i> </span>+91-8750116640</li>
+                <li><span><i class="fa-brands fa-whatsapp"> </i></span>+91-8802042444</li>
+              </ul>
+      </div>
+    </div>
+
+    </div>
+
+    <div class="row mt-5">
+      <div class="career-summary">
+        <h3>CAREER SUMMARY</h3>
+        <hr class="hr mt-2 mb-2">
+
+        <p class="desc">
+          Dynamic professional with a strong background in web development and project coordination. Currently a Web Developer and Project Coordinator at Webkeyindia, with a track record of successfully completing 20+ projects. Skilled in end-to-end project management, web development, and troubleshooting. Experience includes leading web development at Synopsis IAS and shaping online presence at TripZeellaTours. Adept at optimizing social media and ensuring a cohesive brand image. Seeking opportunities to apply technical expertise and project management skills in a dynamic environment.
+        </p>
+        
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-8">
+        <div class="work-exp">
+          <h3>WORK EXPERIENCE</h3>
+          <hr class="hr mt-2 mb-2">
+
+          <h5>Web Developer  <span>(Webkeyindia)</span></h5>
+          <span>November, 2022 - June, 2023</span>
+          <p>At Webkeyindia, a prominent Digital Marketing company, I serve as a Web Developer and Project Coordinator. During my tenure, I have successfully overseen the completion of more than 20 projects, contributing to the company's digital landscape. My role involves not only web development but also coordination and management of various projects.</p>
+
+<p>In addition to the successful project deliveries, I've been actively involved in troubleshooting and resolving errors, particularly in the critical domain of Home projects. This dual responsibility as a Web Developer and Project Coordinator has allowed me to leverage both technical expertise and organizational skills to contribute effectively to the success and seamless operation of Webkeyindia's projects.</p>
+
+           <h5>Web Developer  <span>(TripZeellaTours)</span></h5>
+          <span>Septemper, 2021 - January, 2022</span>
+          <p>During my tenure at TripZeellaTours, I played a pivotal role in crafting their online presence. This involved the end-to-end creation of their website, ensuring a seamless and engaging user experience. Simultaneously, I took charge of content management, curating and updating website content to reflect the latest offerings and information.</p>
+
+<p>In addition to website development and content management, I contributed to TripZeellaTours' marketing efforts by designing eye-catching promotional materials. This included the creation of flyers, posts, and other visual content. My responsibilities extended to optimizing the company's presence on social media platforms, ensuring a cohesive and appealing brand image across various online channels.</p>
+
+           <h5>Web Developer  <span>(Synopsis IAS)</span></h5>
+          <span>March, 2020 - June, 2020</span>
+          <p>In my role as a Website Developer at Synopsis IAS, I spearheaded the complete development of the website, prioritizing a user-friendly and visually appealing online platform. By incorporating modern web design principles and responsive layouts, I aimed to elevate the overall user experience.</p>
+
+          <p>Simultaneously, I took charge of Content Management, meticulously curating all content on the Synopsis IAS website. My responsibilities included ensuring the accuracy, relevance, and alignment of the content with organizational objectives. I consistently maintained up-to-date information, courses, and announcements to keep the website dynamic and informative.</p>
+
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="skill">
+           <h3>Skills</h3>
+          <hr class="hr mt-2 mb-2">
+          <p>
+            <span><strong>Frontend</strong></span>
+            <ul>
+              <li>HTML / HTML 5</li>
+              <li>CSS / CSS 3</li>
+              <li>JavaScript</li>
+              <li>Bootstrap</li>
+              <li>Tailwind</li>
+            </ul>
+          </p>
+
+           <p>
+            <span><strong>Backend</strong></span>
+            <ul>
+              <li>PHP</li>
+              <li>Msql</li>
+            </ul>
+          </p>
+
+          <p>
+            <span><strong>Other</strong></span>
+            <ul>
+              <li>Wordpress</li>
+              <li>Shopify</li>
+            </ul>
+          </p>
+        </div>
+  
+      <div class="education">
+           <h3>Education</h3>
+          <hr class="hr mt-2 mb-2">
+          <p>
+            <ul>
+
+               <li>
+                <strong>BCA (Bachelor of Computer Application)</strong><br>
+                <span>2018-2021</span><br>
+                <span>Maharshi Dyanand University, Rohtak, Haryana</span>
+                
+
+              </li>
+              <br>
+              <li>
+                <strong>12<sup>th</sup></strong><br>
+                <span>2017-2018</span><br>
+                <span>Gagan Bharti Sr. Sec  School, Uttam Nagar, West Delhi, New Delhi 110059</span>
+
+
+              </li>
+              <br>
+
+               <li>
+                <strong>10<sup>th</sup></strong><br>
+                <span>2015-2016</span><br>
+                <span>Gagan Bharti Sr. Sec  School, Vikas Puri, West Delhi, New Delhi 110018</span>
+                
+
+              </li>
+            </ul>
+          </p>
+        </div>
+
+        <div class="hobbies">
+           <h3>Hobbies & Interest</h3>
+          <hr class="hr mt-2 mb-2">
+          <p>
+            <ul>
+              <li>Cricket and Kabaddi</li>
+              <li>Geo-Politics</li>
+              <li>Workout</li>
+
+            </ul>
+          </p>
+        </div>
+      </div>
+    <div class="row">
+        <button width="100" class="m-3 btn btn-success" onclick="printPage()">Print Page</button>
+
+    <script>
+        function printPage() {
+            window.print();
+        }
+    </script>
+    </div>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
 
 ?>
